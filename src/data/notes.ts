28efb2d234ce48ba469b8e4764f33a25f08b5680
azmyar <azmy.arya.rizaldi@gmail.com>
2024-08-14 04:05:04 +0700

    chore: updating notes

--- src/data/notes.ts
+++ src/data/notes.ts
@@ -1,6 +1,5 @@
 const notes = [
-	"Pre-order berlangsung dari tanggal ... dan dilanjut produksi. (estimasi 1 minggu)",
-	"Pengambilan barang dilakukan di SMK-SMAK Bogor.",
+	"Pre-order berlangsung 2 minggu dari tanggal 15 Agustus - 29 Agustus lalu dilanjut proses produksi. Untuk detail timeline bisa di cek di IG @smakbo.swag",
 ];
 
 export { notes };
