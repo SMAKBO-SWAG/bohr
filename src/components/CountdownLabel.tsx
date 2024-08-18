197fba2514d5c1143ae9c98a999e5c8cc8aebb23
azmyar <azmy.arya.rizaldi@gmail.com>
2024-08-18 07:58:57 +0700

    feat: add timer to release the feature accordingly

--- src/components/CountdownLabel.tsx
+++ src/components/CountdownLabel.tsx
@@ -3,19 +3,29 @@ import Countdown from "react-countdown";
 const CountdownLabel = () => {
 	const targetDate = new Date("2024-08-30T00:00:00+07:00");
 
-	return (
-		<div className="w-full bg-dark text-white flex justify-between px-4 py-3 rounded-xl items-center">
-			<span>Pre-order closed in...</span>
-			<span>
-				<Countdown
-					className="text-2xl"
-					date={targetDate}
-					daysInHours={true}
-				/>{" "}
-				Hour(s)
-			</span>
-		</div>
+	const now = new Date();
+	const jakartaTime = new Date(
+		now.toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
 	);
+	const timerAvailableDate = new Date("2024-08-27T00:00:00+07:00");
+
+	if (jakartaTime >= timerAvailableDate) {
+		return (
+			<div className="w-full bg-dark text-white flex justify-between px-4 py-3 rounded-xl items-center">
+				<span>Pre-order closed in...</span>
+				<span>
+					<Countdown
+						className="text-2xl"
+						date={targetDate}
+						daysInHours={true}
+					/>{" "}
+					Hour(s)
+				</span>
+			</div>
+		);
+	}
+
+	return null;
 };
 
 CountdownLabel.displayName = "CountdownLabel";
--- src/data/products.ts
+++ src/data/products.ts
@@ -1,20 +1,12 @@
 import { Product } from "@/types/product";
 
+const now = new Date();
+const jakartaTime = new Date(now.toLocaleString("en-US", { timeZone: "Asia/Jakarta" }));
+const squadAvailableDate = new Date("2024-08-23T18:00:00+07:00");
+
 const products: Product[] = [
-    {
-		id: "squad",
-		name: "Bring Your Squad!",
-		type: "Package",
-		price: 100000,
-		thumbnail: {
-			isNew: false,
-			isPreOrder: true,
-			accent: "#ffffff",
-			accentComplement: "#011B29",
-		},
-	},
 	{
-        id: "fluore",
+		id: "fluore",
 		name: "fluore",
 		type: "Bracelet",
 		price: 25000,
@@ -26,7 +18,7 @@ const products: Product[] = [
 		},
 	},
 	{
-        id: "classic",
+		id: "classic",
 		name: "classic",
 		type: "Bracelet",
 		price: 25000,
@@ -38,5 +30,20 @@ const products: Product[] = [
 		},
 	},
 ];
+
+if (jakartaTime >= squadAvailableDate) {
+	products.unshift({
+		id: "squad",
+		name: "Bring Your Squad!",
+		type: "Package",
+		price: 100000,
+		thumbnail: {
+			isNew: false,
+			isPreOrder: true,
+			accent: "#ffffff",
+			accentComplement: "#011B29",
+		},
+	});
+}
 
 export { products };
