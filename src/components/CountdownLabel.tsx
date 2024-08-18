f2d4710afdbe99f704c7b521db6302d21c598b80
azmyar <azmy.arya.rizaldi@gmail.com>
2024-08-18 20:14:29 +0700

    fix: change timer start date

--- src/components/CountdownLabel.tsx
+++ src/components/CountdownLabel.tsx
@@ -7,25 +7,26 @@ const CountdownLabel = () => {
 	const jakartaTime = new Date(
 		now.toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
 	);
-	const timerAvailableDate = new Date("2024-08-27T00:00:00+07:00");
+	const timerAvailableDate = new Date("2024-08-28T00:00:00+07:00");
 
 	if (jakartaTime >= timerAvailableDate) {
-		return (
-			<div className="w-full bg-dark text-white flex justify-between px-4 py-3 rounded-xl items-center">
-				<span>Pre-order closed in...</span>
-				<span>
-					<Countdown
-						className="text-2xl"
-						date={targetDate}
-						daysInHours={true}
-					/>{" "}
-					Hour(s)
-				</span>
-			</div>
-		);
-	}
+	
+	return (
+		<div className="w-full bg-dark text-white flex justify-between px-4 py-3 rounded-xl items-center">
+			<span>Pre-order closed in...</span>
+			<span>
+				<Countdown
+					className="text-2xl"
+					date={targetDate}
+					daysInHours={true}
+				/>{" "}
+				Hour(s)
+			</span>
+		</div>
+	);
+}
 
-	return null;
+return null
 };
 
 CountdownLabel.displayName = "CountdownLabel";
