dcf178a541038fee3331592454cd9167d624a72f
azmyar <azmy.arya.rizaldi@gmail.com>
2024-08-18 23:41:22 +0700

    feat: change the countdown label bg when its under 24 hours

--- src/components/CountdownLabel.tsx
+++ src/components/CountdownLabel.tsx
@@ -7,26 +7,32 @@ const CountdownLabel = () => {
 	const jakartaTime = new Date(
 		now.toLocaleString("en-US", { timeZone: "Asia/Jakarta" })
 	);
-	const timerAvailableDate = new Date("2024-08-28T00:00:00+07:00");
+	const timerAvailableDate = new Date("2024-08-27T00:00:00+07:00");
+
+	const dateDifference =
+		(targetDate.getTime() - jakartaTime.getTime()) / (1000 * 60 * 60);
 
 	if (jakartaTime >= timerAvailableDate) {
-	
-	return (
-		<div className="w-full bg-dark text-white flex justify-between px-4 py-3 rounded-xl items-center">
-			<span>Pre-order closed in...</span>
-			<span>
-				<Countdown
-					className="text-2xl"
-					date={targetDate}
-					daysInHours={true}
-				/>{" "}
-				Hour(s)
-			</span>
-		</div>
-	);
-}
+		return (
+			<div
+				className={`w-full ${
+					dateDifference <= 24 ? "bg-[#ed4337]" : "bg-dark"
+				} text-white flex justify-between px-4 py-3 rounded-xl items-center`}
+			>
+				<span>Pre-order closed in...</span>
+				<span>
+					<Countdown
+						className="text-2xl"
+						date={targetDate}
+						daysInHours={true}
+					/>{" "}
+					Hour(s)
+				</span>
+			</div>
+		);
+	}
 
-return null
+	return null;
 };
 
 CountdownLabel.displayName = "CountdownLabel";
