6f46bbfd8a728a0bb33d95126c6683a12e078069
azmyar <azmy.arya.rizaldi@gmail.com>
2024-12-07 00:56:08 +0700

    chore: update open and close order date

--- src/data/dates.ts
+++ src/data/dates.ts
@@ -1,5 +1,5 @@
-const openOrderDate = new Date("2024-11-01T23:59:00+07:00");
+const openOrderDate = new Date("2024-12-08T18:00:00+07:00");
 
-const closeOrderDate = new Date("2024-12-15T23:59:00+07:00");
+const closeOrderDate = new Date("2024-12-22T23:59:00+07:00");
 
 export { openOrderDate, closeOrderDate }
